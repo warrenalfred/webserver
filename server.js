75a4d6fe5bffeb46146294df28fa5396de90e2e1
Warren Sturt <wsturt@gmail.com>
2016-11-10 10:48:03 -0500

    Updated launguage

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req, res) {
-	res.send('about us');
+	res.send('about us!');
 });
 
 app.use(express.static(__dirname + '/public'));
